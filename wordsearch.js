28466c339c5cde563a1bcb9d9337c13b30927dd3
kadia5 <kdabraeux@gmail.com>
2021-07-28 21:10:00 +0000

    wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const verticalJoin = letters[0].map((col,index) => letters.map((row) => row[index]).join(''))
+    // const arr
+
+     console.log(verticalJoin)
+    console.log(horizontalJoin)
+
+    if (letters === [] || word === ""){
+       return false;
     }
+ 
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+           return true;
+        } 
+    }
+
+   
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+           return true; 
+        } 
+    }
+    return false;
+
 }
+//find word horizontal and vertically
+//true if words found
+//false if words isnt
+//when its empty?
+
+
+// const rrrr = wordSearch([
+//     ['A', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'D', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'E', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'T', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'I', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'C', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'A', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'L', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ], 'IDENTICAL')
 
 module.exports = wordSearch
